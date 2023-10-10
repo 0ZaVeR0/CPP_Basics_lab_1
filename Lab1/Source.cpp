e785651356e992553c2a46a6a35337510b0f9309
0ZaVeR0 <69377579+0ZaVeR0@users.noreply.github.com>
2023-10-10 22:51:01 +0300

    Update Source.cpp

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -7,18 +7,18 @@ void first() {
 
 	//input and check 
 	while (!check) {
-		cout << "Ââåäèòå Âàø âîçðàñò >> ";
+		cout << "Введите Ваш возраст >> ";
 		cin >> k;
-		if (k < 1) { cout << "Âîçðàñò íå ìîæåò áûòü 0 èëè ìåíüøå\n"; }
-		else if (k > 130) { cout << "Âîçðàñò íå ìîæåò áûòü áîëüøå 130\n"; }
+		if (k < 1) { cout << "Возраст не может быть 0 или меньше\n"; }
+		else if (k > 130) { cout << "Возраст не может быть больше 130\n"; }
 		else { check = true; }
 	}
 
 	//output
 	last_digit = k % 10;
-	if (last_digit == 1) { cout << "Ìíå " << k << " ãîä"; }
-	else if (last_digit < 5) { cout << "Ìíå " << k << " ãîäà"; }
-	else { cout << "Ìíå " << k << " ëåò"; }
+	if (last_digit == 1) { cout << "Мне " << k << " год"; }
+	else if (last_digit < 5) { cout << "Мне " << k << " года"; }
+	else { cout << "Мне " << k << " лет"; }
 
 }
 
@@ -26,19 +26,19 @@ void second() {
 	int points;
 
 	//input
-	cout << "Ââåäèòå êîè÷åñòâî óãëîâ >> ";
+	cout << "Введите коичество углов >> ";
 	cin >> points;
 
 	//output
 	switch (points)
 	{
-	case(3): cout << "Ýòî òðåóãîëüíèê"; break;
-	case(4): cout << "Ýòî êâàäðàò"; break;
-	case(5): cout << "Ýòî ïÿòèóãîëüíèê"; break;
-	case(6): cout << "Ýòî øåñòèóãîëüíèê"; break;
-	case(7): cout << "Ýòî ñåìèóãîëüíèê"; break;
-	case(8): cout << "Ýòî âîñüìèóãîëüíèê"; break;
-	default: cout << "Ýòî íåèçâåñòíàÿ ôèãóðà"; break;
+	case(3): cout << "Это треугольник"; break;
+	case(4): cout << "Это квадрат"; break;
+	case(5): cout << "Это пятиугольник"; break;
+	case(6): cout << "Это шестиугольник"; break;
+	case(7): cout << "Это семиугольник"; break;
+	case(8): cout << "Это восьмиугольник"; break;
+	default: cout << "Это неизвестная фигура"; break;
 	}
 }
 
@@ -50,19 +50,19 @@ void third() {
 
 	//input 
 	while (!correct) {
-		cout << "Ââåäèòå äâà ÷èñëà ÷åðåç ïðîáåë >> ";
+		cout << "Введите два числа через пробел >> ";
 		cin >> a >> b;
 		if (a < b) { correct = true; }
-		else { cout << "Îøèáêà, ïåðâîå ÷èñëî äîëæíî áûòü ìåíüøå âòîðîãî\n"; }
+		else { cout << "Ошибка, первое число должно быть меньше второго\n"; }
 	}
 
 	//input,check and output
 	for (int i = 0; i < 15; i++) {
-		cout << "Ââåäèòå ÷èñëî (" << i+1 << "/15) >> ";
+		cout << "Введите число (" << i+1 << "/15) >> ";
 		cin >> num;
 		if (num >= a && num <= b) { count++; }
 	}
-	cout << "Â äèàïàçîíå îò " << a << " äî " << b << " íàõîäÿòñÿ " << count << " ââåä¸ííûõ ÷èñåë";
+	cout << "В диапазоне от " << a << " до " << b << " находятся " << count << " введённых чисел";
 }
 
 
@@ -73,7 +73,7 @@ int main() {
 	bool running = true;
 
 	while (running) {
-		cout << "\nÂâåäèòå íîìåð çàäàíèÿ >> ";
+		cout << "\nВведите номер задания >> ";
 		cin >> task;
 		switch (task)
 		{
